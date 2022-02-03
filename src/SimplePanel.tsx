1cded1ccd5781e3d01d5de3c86b50bd4caa3e014
littleironical <hardik.kumar18feb@gmail.com>
2022-02-04 01:59:31 +0530

    Matrix UI created

--- src/SimplePanel.tsx
+++ src/SimplePanel.tsx
@@ -1,14 +1,44 @@
 import React from 'react';
 import { PanelProps } from '@grafana/data';
 import { SimpleOptions } from 'types';
-import { Table } from '@grafana/ui';
+import CSS from 'csstype';
 
 interface Props extends PanelProps<SimpleOptions> {}
 
 export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) => {
+  const cardStyle: CSS.Properties = {
+    padding: '16px',
+    textAlign: 'center',
+    borderStyle: 'solid',
+    borderColor: '#f1f1f1',
+    borderWidth: '1px',
+    width: '100px',
+  };
+
+  let actualData: any = [];
+  for (let i = 0; i < 4; i++) {
+    let y: any = data.series[0]['fields'][i]['values'];
+    console.log(y.buffer);
+    actualData[i] = y.buffer;
+  }
+
+  console.log(actualData);
+
+  // SOME ALGORITHM
+
   return (
-    <div className="panel-container" style={{ width: 'auto' }}>
-      <Table data={data.series[0]} height={500} width={width} />
+    <div
+      style={{ height: height, width: width, paddingLeft: 30, overflow: 'auto', overflowY: 'auto', overflowX: 'auto' }}
+    >
+      {actualData.map((value: any, index: any) => (
+        <g key={index} className="row">
+          {actualData[index].map((value1: any, index1: any) => (
+            <div key={index1} className="card" style={cardStyle}>
+              {actualData[index][index1]}
+            </div>
+          ))}
+        </g>
+      ))}
     </div>
   );
 };
